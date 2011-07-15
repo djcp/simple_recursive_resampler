51baec6a1105681d30b88755425edc4d7657d6a4
Daniel Collis-Puro <djcp@collispuro.net>
2011-07-14 20:56:53 -0400

    tweaks.

--- recursive_resample.sh
+++ recursive_resample.sh
@@ -6,7 +6,7 @@ MP3_ROOT=/home/djcp/mp3/
 NEW_ROOT=/home/djcp/mp3_resample/
 MK_DIR="perl -n -e 's/(.+\/).+$/\$1/;print'"
 FILE_TYPE="perl -n -e 's/(.+)\..+$/\$1/;print'"
-LAME_OPTIONS=" -V5 -q2 -v --lowpass 17 -b32 -B192 "
+LAME_OPTIONS="-V4 -q2 --vbr-new"
 
 cd $MP3_ROOT;
 for FILE in `find -type f | grep -iE '(flac|ogg|mp3)$'`; do
@@ -24,7 +24,7 @@ for FILE in `find -type f | grep -iE '(flac|ogg|mp3)$'`; do
 		if [ ! -e $OUTPUT_FILE ] 
 		then
 			ID3_INFO=`~/bin/ogg_id3_info.pl "${FILE}"`
-			TRANSCODE_OGG_COMMAND="/usr/bin/oggdec -o - \"$MP3_ROOT$FILE\" | /usr/bin/lame ${ID3_INFO} ${LAME_OPTIONS} -r - \"$OUTPUT_FILE\""
+			TRANSCODE_OGG_COMMAND="/usr/bin/oggdec -o - \"$MP3_ROOT$FILE\" | /usr/local/bin/lame ${ID3_INFO} ${LAME_OPTIONS} -r - \"$OUTPUT_FILE\""
 			eval $TRANSCODE_OGG_COMMAND 
 		fi
 	fi
@@ -33,7 +33,7 @@ for FILE in `find -type f | grep -iE '(flac|ogg|mp3)$'`; do
 		if [ ! -e $OUTPUT_FILE ] 
 		then
 			ID3_INFO=`mp3info -p "\-\-tt \"%t\" \-\-ta \"%a\" \-\-tl \"%l\" \-\-ty \"%y\" \-\-tn \"%n\"" "$FILE"`
-			TRANSCODE_MP3_COMMAND="/usr/bin/lame ${ID3_INFO} ${LAME_OPTIONS} \"${FILE}\" \"${OUTPUT_FILE}\""
+			TRANSCODE_MP3_COMMAND="/usr/local/bin/lame ${ID3_INFO} ${LAME_OPTIONS} \"${FILE}\" \"${OUTPUT_FILE}\""
 			eval $TRANSCODE_MP3_COMMAND
 		fi
 	fi
@@ -42,7 +42,7 @@ for FILE in `find -type f | grep -iE '(flac|ogg|mp3)$'`; do
 		if [ ! -e $OUTPUT_FILE ] 
 		then
 			ID3_INFO=`~/bin/flac_id3_info.pl "${FILE}"`
-			TRANSCODE_FLAC_COMMAND="/usr/bin/flac -dc \"${FILE}\" | /usr/bin/lame ${ID3_INFO} ${LAME_OPTIONS} - \"${OUTPUT_FILE}\""
+			TRANSCODE_FLAC_COMMAND="/usr/bin/flac -dc \"${FILE}\" | /usr/local/bin/lame ${ID3_INFO} ${LAME_OPTIONS} - \"${OUTPUT_FILE}\""
 			eval $TRANSCODE_FLAC_COMMAND
 		fi
 	fi
